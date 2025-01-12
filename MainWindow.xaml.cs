37450c264e760819e76d3ad25eb3f10a1c0fd00b
marshall-bits <kz_@hotmail.es>
2025-01-12 19:07:23 +0100

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace draw_mode_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point? _startPoint = null;
+        private Line? _currentLine = null;
+        private Brush? _currentBrush = Brushes.LightYellow;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                _startPoint = e.GetPosition(canvas);
+
+                _currentLine = new Line
+                {
+                    Stroke = _currentBrush,
+                    StrokeThickness = 5,
+                    X1 = _startPoint.Value.X,
+                    Y1 = _startPoint.Value.Y,
+                    X2 = _startPoint.Value.X,
+                    Y2 = _startPoint.Value.Y
+                };
+
+                canvas.Children.Add(_currentLine);
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_startPoint.HasValue && _currentLine != null)
+            {
+                Point currentPosition = e.GetPosition(canvas);
+                _currentLine.X2 = currentPosition.X;
+                _currentLine.Y2 = currentPosition.Y;
+            }
+        }
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_startPoint.HasValue && _currentLine != null)
+            {
+                Point endPoint = e.GetPosition(canvas);
+                _currentLine.X2 = endPoint.X;
+                _currentLine.Y2 = endPoint.Y;
+                _startPoint = null;
+                _currentLine = null;
+            }
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    this.Close();
+                    break;
+
+                case Key.D1:
+                case Key.NumPad1:
+                    _currentBrush = Brushes.LightYellow; 
+                    break;
+
+                case Key.D2:
+                case Key.NumPad2:
+                    _currentBrush = Brushes.LightGreen; 
+                    break;
+
+                case Key.D3:
+                case Key.NumPad3:
+                    _currentBrush = Brushes.LightCoral; 
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Draw mode
+
+## Description
+
+This is a simple app for windows that allows you to draw lines on the screen over other apps. It's written in C# and uses the DotNet framework.
+
+Fork and clone the repository to your local machine. 
+You can run it using the following command:
+
+```bash
+dotnet run
+```
+
+Or create a release build using the following command:
+
+```bash
+dotnet publish -c Release -r win-x64 --self-contained true
+```
+
+## How does it work?
+
+- Draw straight lines on the screen using the mouse
+- Change the color of the line by pressing the keys 1, 2, 3
+- Close the program by pressing the key escape
